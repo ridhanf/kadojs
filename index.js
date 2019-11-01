0652097f86e85e85d6f7a1f70db264de33116139
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2019-11-02 01:34:17 +0700

    add eslintrc, fix syntax error, clean code index.js and test.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,7 +9,7 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/give', (req, res) => res.render('pages/give'))
-  .get('/receive', (req,res) => res.render('pages/receive'))
-  .get('/about', (req,res) => res.render('pages/about'))
-  .get('/account', (req,res) => res.render('pages/account')) 
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/receive', (req, res) => res.render('pages/receive'))
+  .get('/about', (req, res) => res.render('pages/about'))
+  .get('/account', (req, res) => res.render('pages/account'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
--- test.js
+++ test.js
@@ -3,14 +3,18 @@ const request = require('request');
 const test = require('tape');
 
 // Start the app
-const env = Object.assign({}, process.env, {PORT: 5000});
-const child = spawn('node', ['index.js'], {env});
+
+// const env = Object.assign({ }, process.env, { PORT: 5000 });
+// change to code below
+const env = { PORT: 5000, ...process.env };
+
+const child = spawn('node', ['index.js'], { env });
 
 test('responds to requests', (t) => {
   t.plan(4);
 
   // Wait until the server is ready
-  child.stdout.on('data', _ => {
+  child.stdout.on('data', () => {
     // Make a request to our app
     request('http://127.0.0.1:5000', (error, response, body) => {
       // stop the server
@@ -21,8 +25,8 @@ test('responds to requests', (t) => {
       // Successful response
       t.equal(response.statusCode, 200);
       // Assert content checks
-      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
+      t.notEqual(body.indexOf('<title>Node.js Getting Started on Heroku</title>'), -1);
+      t.notEqual(body.indexOf('Getting Started with Node on Heroku'), -1);
     });
   });
 });
